a2d2a227f81961df041d68393efc9417cfaee892
kewang <cpckewang@gmail.com>
2014-04-20 15:33:32 +0800

    let output html pretty

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.locals.pretty = true;
+
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
