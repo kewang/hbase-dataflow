17145b2c473404ccfc70e7802bddc15b83c0a96b
kewang <cpckewang@gmail.com>
2014-05-11 16:47:55 +0800

    create table & bind to select control

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -1,3 +1,15 @@
 function dataflow($scope){
+  $scope.tableList = [];
 
+  $scope.createTable = function(){
+    name = prompt("Create a new table");
+
+    this.tableList.push({
+      "name": name
+    });
+  }
+
+  $scope.changeTable = function(){
+  
+  }
 }
