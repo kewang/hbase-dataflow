5336921e6bd7718e676ffa99407111953db0af2d
kewang <cpckewang@gmail.com>
2014-05-11 17:22:55 +0800

    restrict variable scope

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -2,8 +2,7 @@ function dataflow($scope){
   $scope.tableList = [];
 
   $scope.createTable = function(){
-    name = prompt("Create a new table");
-
+    var name = prompt("Create a new table");
     var table = new Table(name);
 
     console.log(table);
