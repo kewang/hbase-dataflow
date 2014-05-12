7cbf87c97c1c89d1c91627fb639b0618983aa933
kewang <cpckewang@gmail.com>
2014-05-12 13:08:36 +0800

    use ng-table instead of ng-grid

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -1,15 +1,21 @@
-var app = angular.module("hbase-dataflow-app", ["ngGrid"]);
+var app = angular.module("hbase-dataflow-app", ["ngTable"]);
 
-app.controller("dataflow", function($scope){
-  $scope.myData = [
+app.controller("dataflow", function($scope, ngTableParams){
+  $scope.data = [
     {name: "Moroni", age: 50},
-    {name: "Tiancum", age: 43},
-    {name: "Jacob", age: 27},
     {name: "Nephi", age: 29},
     {name: "Enos", age: 34}
   ];
 
-  $scope.gridOptions = { data: 'myData' };
+  $scope.tableParams = new ngTableParams({
+    page: 1,
+    count: 10
+  }, {
+    total: $scope.data.length,
+    getData: function($defer, params){
+      $defer.resolve($scope.data.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+    }
+  });
 
   $scope.tableList = [];
 
