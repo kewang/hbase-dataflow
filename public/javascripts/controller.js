350500046890c86d9015b91b6e0d0010ae7329ab
kewang <cpckewang@gmail.com>
2014-05-16 23:48:27 +0800

    add modal dialog to create rowkey & cq

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -3,20 +3,26 @@ var app = angular.module("hbase-dataflow-app", []);
 app.controller("dataflow", function($scope){
   $scope.cqs = [];
   $scope.tableList = [];
+  $scope.tmp_cqs = [];
 
   $scope.createTable = function() {
     var name = prompt("Create a new table");
     var t = new Table(name);
 
     this.tableList.push(t);
-
-    console.log(this.tableList);
   };
 
   $scope.addCQ = function() {
-    $scope.cqs.push({
-      name: "aaa",
-      value: "asdfasdf"
-    });
+    this.tmp_cqs.push({});
+  };
+
+  $scope.createRowkeyAndCQ = function() {
+    this.selectTable2.createRowkey(this.tmp_rk);
+
+    for(var i=0;i<this.tmp_cqs.length;i++){
+      this.selectTable2.createCQ(this.tmp_rk, this.tmp_cqs[i].name, this.tmp_cqs[i].value);
+    }
+
+    $("#create-qualifiers-dialog").modal("hide");
   };
 });
--- public/javascripts/models/Table.js
+++ public/javascripts/models/Table.js
@@ -3,10 +3,28 @@ function Table(name){
   this.rowkeys = [];
 }
 
-Table.prototype.createRow = function(rowkey, qualifier, value) {
-  this.rowkeys[rowkey].qualifiers = [];
-
-  this.rowkeys[rowkey].qualifiers.push({
-    qualifier: value
+Table.prototype.createRowkey = function(rowkey) {
+  this.rowkeys.push({
+    "rowkey": rowkey,
+    "cqs": []
   });
+};
+
+Table.prototype.createCQ = function(rowkey, cq, value) {
+  var found;
+
+  for(var i=0;i<this.rowkeys.length;i++){
+    if(this.rowkeys[i].rowkey === rowkey){
+      found = this.rowkeys[i];
+
+      break;
+    }
+  }
+
+  if(found){
+    found.cqs.push({
+      "name": cq,
+      "value": value
+    });
+  }
 };
