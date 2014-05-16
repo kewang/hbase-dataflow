f94b9cbaf30a0211795ecdb50c20a000208924d1
kewang <cpckewang@gmail.com>
2014-05-16 22:07:17 +0800

    remove unused variable

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -1,20 +1,14 @@
 var app = angular.module("hbase-dataflow-app", []);
 
 app.controller("dataflow", function($scope){
-  $scope.data = [
-    {name: "Moroni", age: 50},
-    {name: "Nephi", age: 29},
-    {name: "Enos", age: 34}
-  ];
-
   $scope.cqs = [];
   $scope.tableList = [];
 
   $scope.createTable = function() {
     var name = prompt("Create a new table");
-    var table = new Table(name);
+    var t = new Table(name);
 
-    this.tableList.push(table);
+    this.tableList.push(t);
 
     console.log(this.tableList);
   };
