9f5865de48c895925dee1210317770841a29b93a
kewang <cpckewang@gmail.com>
2014-05-18 22:57:48 +0800

    complete rough put data

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -4,7 +4,8 @@ app.controller("dataflow", function($scope){
   $scope.cqs = [];
   $scope.tableList = [];
   $scope.tmp_cqs = [];
-  $scope.currentTable;
+  $scope.current_cqs = [];
+  $scope.current_rowkeys;
 
   $scope.createTable = function() {
     var name = prompt("Create a new table");
@@ -28,6 +29,48 @@ app.controller("dataflow", function($scope){
   };
 
   $scope.showTable = function() {
-    this.currentTable = this.selectTable1;
+    var rowkeys = this.selectTable1.rowkeys;
+
+    this.current_rowkeys = [];
+
+    for(var i=0;i<rowkeys.length;i++){
+      for(var j=0;j<rowkeys[i].cqs.length;j++){
+        var cq = rowkeys[i].cqs[j];
+
+        // not found the same CQ
+        if(this.current_cqs.indexOf(cq.name) == -1){
+          this.current_cqs.push(cq.name);
+        }
+      }
+    }
+
+    for(var i=0;i<rowkeys.length;i++){
+      var cqs = [];
+
+      for(var j=0;j<this.current_cqs.length;j++){
+        var found = false;
+
+        for(var k=0;k<rowkeys[i].cqs.length;k++){
+          var cq = rowkeys[i].cqs[k];
+
+          if(this.current_cqs[j] === cq.name){
+            cqs.push(cq.value);
+
+            found=true;
+
+            break;
+          }
+        }
+
+        if(!found){
+          cqs.push(null);
+        }
+      }
+
+      this.current_rowkeys.push({
+        "rowkey": rowkeys[i].rowkey,
+        "cqs": cqs
+      });
+    }
   };
 });
