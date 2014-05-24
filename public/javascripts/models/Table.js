e262ea6130c9ed37e1db4814e219a5c0e4f70034
kewang <cpckewang@gmail.com>
2014-05-24 15:42:43 +0800

    refactor controller

--- public/javascripts/models/Table.js
+++ public/javascripts/models/Table.js
@@ -1,6 +1,8 @@
 function Table(name){
   this.name = name;
   this.rowkeys = [];
+  this.fullRowkeys;
+  this.fullCQs;
 }
 
 Table.prototype.createRowkey = function(rowkey) {
@@ -27,4 +29,52 @@ Table.prototype.createCQ = function(rowkey, cq, value) {
       "value": value
     });
   }
+};
+
+Table.prototype.getFullTable = function() {
+  var current_rowkeys = [];
+  var current_cqs = [];
+
+  for(var i=0;i<this.rowkeys.length;i++){
+    for(var j=0;j<this.rowkeys[i].cqs.length;j++){
+      var cq = this.rowkeys[i].cqs[j];
+
+      // not found the same CQ
+      if(current_cqs.indexOf(cq.name) == -1){
+        current_cqs.push(cq.name);
+      }
+    }
+  }
+
+  for(var i=0;i<this.rowkeys.length;i++){
+    var cqs = [];
+
+    for(var j=0;j<current_cqs.length;j++){
+      var found = false;
+
+      for(var k=0;k<this.rowkeys[i].cqs.length;k++){
+        var cq = this.rowkeys[i].cqs[k];
+
+        if(current_cqs[j] === cq.name){
+          cqs.push(cq.value);
+
+          found=true;
+
+          break;
+        }
+      }
+
+      if(!found){
+        cqs.push(null);
+      }
+    }
+
+    current_rowkeys.push({
+      "rowkey": this.rowkeys[i].rowkey,
+      "cqs": cqs
+    });
+  }
+
+  this.fullRowkeys = current_rowkeys;
+  this.fullCQs = current_cqs;
 };
