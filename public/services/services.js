cc5c290fcac0f33d35675dd249b36ba2ce77e7cb
kewang <cpckewang@gmail.com>
2014-06-05 23:27:46 +0800

    move Table from models to services

--- public/services/services.js
+++ public/services/services.js
@@ -6,10 +6,99 @@ app.factory("Table", function() {
 
   function Table(name){
     this.name = name;
+    this.rowkeys = [];
+    this.fullRowkeys;
+    this.fullCQs;
   }
 
   Table.prototype.getName = function(){
     return this.name;
+  };
+
+  Table.prototype.createRowkey = function(rowkey) {
+    this.rowkeys.push({
+      "rowkey": rowkey,
+      "cqs": []
+    });
+  };
+
+  Table.prototype.createCQ = function(rowkey, cq, value) {
+    var found;
+
+    for(var i=0;i<this.rowkeys.length;i++){
+      if(this.rowkeys[i].rowkey === rowkey){
+        found = this.rowkeys[i];
+
+        break;
+      }
+    }
+
+    if(found){
+      found.cqs.push({
+        "name": cq,
+        "value": value
+      });
+    }
+  };
+
+  Table.prototype.buildFullTable = function() {
+    var current_rowkeys = [];
+    var current_cqs = [];
+
+    for(var i=0;i<this.rowkeys.length;i++){
+      for(var j=0;j<this.rowkeys[i].cqs.length;j++){
+        var cq = this.rowkeys[i].cqs[j];
+
+        // not found the same CQ
+        if(current_cqs.indexOf(cq.name) == -1){
+          current_cqs.push(cq.name);
+        }
+      }
+    }
+
+    for(var i=0;i<this.rowkeys.length;i++){
+      var cqs = [];
+
+      for(var j=0;j<current_cqs.length;j++){
+        var found = false;
+
+        for(var k=0;k<this.rowkeys[i].cqs.length;k++){
+          var cq = this.rowkeys[i].cqs[k];
+
+          if(current_cqs[j] === cq.name){
+            cqs.push(cq.value);
+
+            found=true;
+
+            break;
+          }
+        }
+
+        if(!found){
+          cqs.push(null);
+        }
+      }
+
+      current_rowkeys.push({
+        "rowkey": this.rowkeys[i].rowkey,
+        "cqs": cqs
+      });
+    }
+
+    this.fullRowkeys = current_rowkeys;
+    this.fullCQs = current_cqs;
+  };
+
+  Table.prototype.setRowkeys = function(rowkeys) {
+    this.rowkeys = rowkeys;
+  };
+
+  Table.prototype.getFullRowkeys = function() {
+    return this.fullRowkeys;
+  };
+
+  Table.prototype.getFullCQs = function() {
+    return this.fullCQs;
   };
 
   Table.create = function(table){
