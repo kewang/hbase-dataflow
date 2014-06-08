d96b2ed643794be4500a6c0ef0577078014f9265
kewang <cpckewang@gmail.com>
2014-06-08 16:37:24 +0800

    try to add text-replace to grunt, but fail

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,14 @@ module.exports = function(grunt){
         files: [{
           expand: true,
           cwd: "views/",
-          src: ["**/*.jade", "!layout.jade"],
+          src: ["**/*.jade", "!layout.jade", "!index.jade"],
+          dest: "build/templates/",
+          ext: "",
+          extDot: "first"
+        }, {
+          expand: true,
+          cwd: "views/",
+          src: ["index.jade"],
           dest: "build/templates/",
           ext: ".htm",
           extDot: "first"
@@ -22,6 +29,16 @@ module.exports = function(grunt){
           cwd: "public/",
           src: ["**/*.js", "**/*.css"],
           dest: "build/templates/"
+        }]
+      }
+    },
+    "replace": {
+      htm: {
+        src: ["build/templates/controllers/controllers.js"],
+        dest: "build/templates/controllers/",
+        replacements: [{
+          from: /^(\s+templateUrl: "[\w\/]+)(",)$/g,
+          to: "$1.htm$2"
         }]
       }
     },
@@ -39,7 +56,9 @@ module.exports = function(grunt){
 
   grunt.loadNpmTasks("grunt-contrib-jade");
   grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.loadNpmTasks("grunt-text-replace");
   grunt.loadNpmTasks("grunt-gh-pages");
 
+  //grunt.registerTask("default", ["jade", "copy", "replace", "gh-pages"]);
   grunt.registerTask("default", ["jade", "copy", "gh-pages"]);
 };
