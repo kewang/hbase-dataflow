28c84c4372302162806cb196157bab58fb0a6620
kewang <cpckewang@gmail.com>
2014-06-08 06:37:43 +0000

    Updates

---
+++ controllers/controllers.js
@@ -0,0 +1,203 @@
+var app = angular.module("hbase-dataflow-app", ["hbase-dataflow-app.services", "ui.bootstrap"]);
+
+app.controller("TableCtrl", function($scope, $modal, Table){
+  $scope.tables = Table.findAll();
+
+  $scope.createTable = function() {
+    var name = prompt("Create a new table");
+
+    if(name){
+      var t = new Table(name);
+
+      Table.create(t);
+    }else{
+      alert("Please input a table name");
+    }
+  };
+
+  $scope.exportTables = function() {
+    var MIMETYPE = "application/json";
+    var tmpTables = angular.copy($scope.tables);
+
+    for(var i=0;i<tmpTables.length;i++){
+      var table = tmpTables[i];
+
+      delete table.fullRowkeys;
+      delete table.fullCQs;
+    }
+
+    var exportData = angular.toJson(tmpTables);
+    var blob = new Blob([exportData], {type: MIMETYPE});
+    var a = document.createElement("a");
+
+    window.URL = window.webkitURL || window.URL;
+
+    a.download = "export.json";
+    a.href = window.URL.createObjectURL(blob);
+    a.dataset.downloadurl = [MIMETYPE, a.download, a.href].join(":");
+
+    a.click();
+  };
+
+  $scope.showImportTablesDialog = function(){
+    var modalInstance = $modal.open({
+      templateUrl: "/includes/import_tables_dialog",
+      controller: "ImportTablesDialogCtrl",
+      size: "lg"
+    });
+  };
+});
+
+app.controller("ImportTablesDialogCtrl", function($scope, $modalInstance, Table){
+  $scope.tables = Table.findAll();
+
+  $scope.importTables = function() {
+    var file = $("#fileChoose")[0].files[0];
+    var reader = new FileReader();
+
+    reader.onloadend = function(e) {
+      var data = JSON.parse(e.target.result);
+
+      for(var i=0;i<data.length;i++){
+        var tmpTable = new Table(data[i].name);
+
+        tmpTable.setRowkeys(data[i].rowkeys);
+
+        tmpTable.buildFullTable();
+
+        $scope.$apply(function(){
+          $scope.tables.push(tmpTable);
+        });
+      }
+
+      $modalInstance.close();
+    }
+
+    reader.readAsText(file);
+  };
+});
+
+app.controller("CreateRowCtrl", function($scope, $modal, Table){
+  $scope.tables = Table.findAll();
+
+  $scope.showCreateRowDialog = function(){
+    var modalInstance = $modal.open({
+      templateUrl: "/includes/create_row_dialog",
+      controller: "CreateRowDialogCtrl",
+      size: "lg",
+      resolve: {
+        createTable: function(){
+          return $scope.createTable;
+        }
+      }
+    });
+  };
+});
+
+app.controller("CreateRowDialogCtrl", function($scope, $modalInstance, createTable, Operation){
+  $scope.table = createTable;
+  $scope.form = {};
+  $scope.form.cqs = [];
+
+  $scope.addCQ = function(){
+    $scope.form.cqs.push({});
+  };
+
+  $scope.create = function() {
+    // create row key and cq
+    $scope.table.createRowkey($scope.form.rowKey);
+
+    for(var i=0;i<$scope.form.cqs.length;i++){
+      var name = $scope.form.cqs[i].name;
+      var value = $scope.form.cqs[i].value;
+
+      $scope.table.createCQ($scope.form.rowKey, name, value);
+    }
+
+    var o = new Operation($scope.form.operationTitle);
+
+    Operation.create(o);
+
+    // clear form field
+    $scope.form.rowKey = "";
+    $scope.form.cqs = [];
+    $scope.form.operationTitle = "";
+
+    $scope.table.buildFullTable();
+
+    $modalInstance.close();
+  };
+});
+
+app.controller("UpdateRowCtrl", function($scope, $modal, Table){
+  $scope.tables = Table.findAll();
+
+  $scope.showUpdateRowDialog = function(){
+    var modalInstance = $modal.open({
+      templateUrl: "/includes/update_row_dialog",
+      controller: "UpdateRowDialogCtrl",
+      size: "lg",
+      resolve: {
+        updateTable: function(){
+          return $scope.updateTable;
+        }
+      }
+    });
+  };
+});
+
+app.controller("UpdateRowDialogCtrl", function($scope, $modalInstance, updateTable, Operation){
+  $scope.table = updateTable;
+  $scope.form = {};
+
+  $scope.addCQ = function(){
+    $scope.form.rowKey.cqs.push({});
+  };
+
+  $scope.update = function() {
+    $scope.table.removeByRowkey($scope.form.rowKey);
+
+    // create row key and cq
+    $scope.table.createRowkey($scope.form.rowKey.rowkey);
+
+    for(var i=0;i<$scope.form.rowKey.cqs.length;i++){
+      var name = $scope.form.rowKey.cqs[i].name;
+      var value = $scope.form.rowKey.cqs[i].value;
+
+      $scope.table.createCQ($scope.form.rowKey.rowkey, name, value);
+    }
+
+    var o = new Operation($scope.form.operationTitle);
+
+    Operation.create(o);
+
+    // clear form field
+    $scope.form.rowKey = {};
+    $scope.form.operationTitle = "";
+
+    $scope.table.buildFullTable();
+
+    $modalInstance.close();
+  };
+});
+
+app.controller("OperationCtrl", function($scope, $modal, Operation){
+  $scope.operations = Operation.findAll();
+
+  $scope.showOperationDialog = function(operation){
+    var modalInstance = $modal.open({
+      templateUrl: "/includes/operation_dialog",
+      controller: "OperationDialogCtrl",
+      size: "lg",
+      resolve: {
+        operation: function(){
+          return operation;
+        }
+      }
+    });
+  };
+});
+
+app.controller("OperationDialogCtrl", function($scope, $modalInstance, operation){
+  $scope.operation = operation;
+});
---
+++ services/services.js
@@ -0,0 +1,165 @@
+var app = angular.module("hbase-dataflow-app.services", []);
+
+// imitate http://www.bennadel.com/blog/2527-defining-instantiatable-classes-in-the-angularjs-dependency-injection-framework.htm
+app.factory("Table", function() {
+  var entities = [];
+
+  function Table(name){
+    this.name = name;
+    this.rowkeys = [];
+    this.fullRowkeys;
+    this.fullCQs;
+  }
+
+  Table.prototype.getName = function(){
+    return this.name;
+  };
+
+  Table.prototype.createRowkey = function(rowkey) {
+    this.rowkeys.push({
+      "rowkey": rowkey,
+      "cqs": []
+    });
+  };
+
+  Table.prototype.getRowkeys = function(){
+    return this.rowkeys;
+  };
+
+  Table.prototype.createCQ = function(rowkey, cq, value) {
+    var found;
+
+    for(var i=0;i<this.rowkeys.length;i++){
+      if(this.rowkeys[i].rowkey === rowkey){
+        found = this.rowkeys[i];
+
+        break;
+      }
+    }
+
+    if(found){
+      found.cqs.push({
+        "name": cq,
+        "value": value
+      });
+    }
+  };
+
+  Table.prototype.buildFullTable = function() {
+    var current_rowkeys = [];
+    var current_cqs = [];
+
+    for(var i=0;i<this.rowkeys.length;i++){
+      for(var j=0;j<this.rowkeys[i].cqs.length;j++){
+        var cq = this.rowkeys[i].cqs[j];
+
+        // not found the same CQ
+        if(current_cqs.indexOf(cq.name) == -1){
+          current_cqs.push(cq.name);
+        }
+      }
+    }
+
+    for(var i=0;i<this.rowkeys.length;i++){
+      var cqs = [];
+
+      for(var j=0;j<current_cqs.length;j++){
+        var found = false;
+
+        for(var k=0;k<this.rowkeys[i].cqs.length;k++){
+          var cq = this.rowkeys[i].cqs[k];
+
+          if(current_cqs[j] === cq.name){
+            cqs.push(cq.value);
+
+            found=true;
+
+            break;
+          }
+        }
+
+        if(!found){
+          cqs.push(null);
+        }
+      }
+
+      current_rowkeys.push({
+        "rowkey": this.rowkeys[i].rowkey,
+        "cqs": cqs
+      });
+    }
+
+    this.fullRowkeys = current_rowkeys;
+    this.fullCQs = current_cqs;
+  };
+
+  Table.prototype.setRowkeys = function(rowkeys) {
+    this.rowkeys = rowkeys;
+  };
+
+  Table.prototype.getFullRowkeys = function() {
+    return this.fullRowkeys;
+  };
+
+  Table.prototype.getFullCQs = function() {
+    return this.fullCQs;
+  };
+
+  Table.prototype.removeByRowkey = function(rowkey){
+    for(var i = 0;i<this.rowkeys.length;i++){
+      var r = this.rowkeys[i];
+
+      if(r.rowkey === rowkey.rowkey){
+        this.rowkeys.splice(i, 1);
+
+        break;
+      }
+    }
+  };
+
+  Table.create = function(table){
+    entities.push(table);
+  };
+
+  Table.findAll = function(){
+    return entities;
+  };
+
+  Table.findByName = function(name){
+    var found;
+
+    for(entity in entities){
+      if(entity.name === name){
+        found = entity;
+
+        break;
+      }
+    }
+
+    return found;
+  };
+
+  return Table;
+});
+
+app.factory("Operation", function() {
+  var entities = [];
+
+  function Operation(title){
+    this.title = title;
+  }
+
+  Operation.prototype.getTitle = function(){
+    return this.title;
+  };
+
+  Operation.create = function(operation){
+    entities.push(operation);
+  };
+
+  Operation.findAll = function(){
+    return entities;
+  };
+
+  return Operation;
+});
