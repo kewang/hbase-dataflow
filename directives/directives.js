2acdb80f5a5704366b3221f728f0ae11a7090ba1
kewang <cpckewang@gmail.com>
2014-06-24 16:09:51 +0000

    Updates

--- directives/directives.js
+++ directives/directives.js
@@ -5,7 +5,7 @@ var app = angular.module("hbase-dataflow-app");
 app.directive('hbaseTable', function() {
   return {
     restrict: 'E',
-    templateUrl: '/includes/hbase_table',
+    templateUrl: 'includes/hbase_table',
     scope: {
       table: "="
     }
