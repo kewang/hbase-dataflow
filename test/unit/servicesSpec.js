9dd5be30d84a073da9fe8a9b51a96dfb0b07029e
kewang <cpckewang@gmail.com>
2014-07-27 11:58:44 +0800

    add more unit tests

--- test/unit/servicesSpec.js
+++ test/unit/servicesSpec.js
@@ -89,6 +89,7 @@ describe("Services", function() {
       .addColumn("testFamily2:testColumn4", "asdfasfsafd", 99999)
       .addColumn("testFamily2:testColumn4", "mitake", 100000);
 
+    expect(testRow.getKey()).toBe("testRow");
     expect(testRow.findColumnByName("testFamily:testColumn")).not.toBe(null);
     expect(testRow.findColumnByName("testFamily:testColumn").getValue().getValue()).toBe("world");
     expect(testRow.findColumnByName("testFamily:testColumnNotFound")).toBe(null);
