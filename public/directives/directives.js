556096d3bb46f5af5898f3e2e229167e32dee33a
kewang <cpckewang@gmail.com>
2014-08-02 17:56:19 +0800

    complete simple hbase-table directive

--- public/directives/directives.js
+++ public/directives/directives.js
@@ -12,18 +12,58 @@ app.directive('hbaseTable', function() {
     link: function(scope, elem, attrs) {
       // deep watch attrs.table
       scope.$watch(attrs.table, function(table) {
-        function isTheSameColumn(columns, column) {
-          var found = false;
+        function findColumnIndex(columns, column) {
+          var found = -1;
 
           for (var i = 0; i < columns.length; i++) {
             if (columns[i].getName() === column.getName()) {
-              found = true;
+              found = i;
 
               break;
             }
           }
 
           return found;
+        }
+
+        function buildColumns(rows) {
+          var returnColumns = [];
+
+          for (var i = 0; i < rows.length; i++) {
+            var row = rows[i];
+            var families = row.getColumns();
+            var values = [];
+
+            for (var j = 0; j < families.length; j++) {
+              var family = families[j];
+              var columns = family.getColumns();
+
+              for (var k = 0; k < columns.length; k++) {
+                // copy a virtual variable
+                var column = angular.copy(columns[k]);
+
+                column.setName(family.getName() + ":" + column.getName());
+
+                var columnIndex = findColumnIndex(returnColumns, column);
+
+                if (columnIndex === -1) {
+                  returnColumns.push(column);
+                }
+              }
+            }
+          }
+
+          return returnColumns;
+        }
+
+        function createFixedLengthArray(size) {
+          var array = [];
+
+          for (var i = 0; i < size; i++) {
+            array.push({});
+          }
+
+          return array;
         }
 
         if (table) {
@@ -33,28 +73,31 @@ app.directive('hbaseTable', function() {
             scope.columns = [];
             scope.rows = [];
 
+            scope.columns = buildColumns(rows);
+
             for (var i = 0; i < rows.length; i++) {
               var row = rows[i];
               var families = row.getColumns();
-              var values = [];
+              var values = createFixedLengthArray(scope.columns.length);
 
               for (var j = 0; j < families.length; j++) {
                 var family = families[j];
                 var columns = family.getColumns();
 
                 for (var k = 0; k < columns.length; k++) {
-                  // copy a virtual variable
                   var column = angular.copy(columns[k]);
 
                   column.setName(family.getName() + ":" + column.getName());
 
-                  var theSameColumn = isTheSameColumn(scope.columns, column);
+                  var columnIndex = findColumnIndex(scope.columns, column);
 
-                  if (!theSameColumn) {
-                    scope.columns.push(column);
+                  for (var l = 0; l < scope.columns.length; l++) {
+                    if (l === columnIndex) {
+                      values[l] = values[l] && column.getValue();
+                    } else {
+                      values[l] = values[l] || null;
+                    }
                   }
-
-                  values.push(column.getValue());
                 }
               }
 
